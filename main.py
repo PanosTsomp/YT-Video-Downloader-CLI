1ac15d2c3cfbef48b9524aab129022744a9b8c81
Panagiotis Tsompanoglou <panostsomp0211@gmail.com>
2026-03-07 18:26:25 +0200

    Added menus to decouple logic

--- main.py
+++ main.py
@@ -1,5 +1,37 @@
+StartupQuestion = "Please Choose An Option:"
+options= [
+    "Menu",
+    "Options",
+    "About us"
+    "Exit"
+]
+
+MenuTitle = "What Do You Want To Do?"
+options= [
+    "Download Audio",
+    "Download Video",
+    "Download Audio and Video",
+    "Download Subtitles",
+    "Download Thumbmail",
+    "Download Playlist only Audio",
+    "Download Playlist only Video",
+    "Download Playlist Video and Audio",
+    "",
+    "",
+    "",
+    "",
+]
+
+AudioDownload = "What type of file format do you want the Audio to be"
+
+
 def main():
-    print("Hello from yt-video-downloader-cli!")
+    print("\nWelcome to Youtube Video Downloader cli! (YVD - cli)\n\n")
+
+    
+
+
+    #Menu
 
 
 if __name__ == "__main__":
